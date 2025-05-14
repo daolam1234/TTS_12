8bde1def6cc3132f3bdc71e0b21d87613e87a423
viet261205 <viet22331111@gmail.com>
2025-05-14 09:00:38 +0700

    router

--- base-fe/src/App.tsx
+++ base-fe/src/App.tsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/button"
+import { useRoutes } from "react-router-dom"
 
 function App() {
+
+  const router = useRoutes([
+    {
+      path:'/',
+      element: <div>home</div>
+    }
+  ])
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
+      {router}
     </div>
   )
 }
