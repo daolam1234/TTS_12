ba7ac1b4515f3e39d7c1d9c2ff928d6063cd3475
daolam1234 <lam15081999@gmail.com>
2025-06-26 00:05:13 +0700

    fix login

--- base-fe/src/router/authRouter.tsx
+++
@@ -1,9 +0,0 @@
-// src/routes/authRouter.tsx
-
-import Login from "@/pages/auth/login";
-import type { RouteObject } from "react-router-dom";
-
-export const authRouter: RouteObject = {
-  path: "/login",
-  element: <Login />,
-};
--- base-fe/src/services/auth.ts
+++ base-fe/src/services/auth.ts
@@ -2,13 +2,9 @@ import instanceAxios from "@/utils/axios";
 
 export const authService = {
   login: async (data: { email: string; password: string }) => {
-    // Gọi đúng endpoint json-server
-    const res = await instanceAxios.post(`/auth/login`);
-    const users = res.data;
-    if (!users.length) throw new Error("Email không tồn tại!");
-    const user = users[0];
-    if (user.password !== data.password) throw new Error("Mật khẩu không đúng!");
-    // Trả về dữ liệu giống backend thật
-    return { token: "dummy-token", user };
+    const res = await instanceAxios.post(`/auth/login`, data);
+    console.log(data);
+    
+    return res.data;
   },
 };
